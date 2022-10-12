24c7e49b4dc728a59917060e044c9b7c5689a7eb
Eduardo Nunes <50140955+EduardoNunes5@users.noreply.github.com>
2022-10-12 20:23:45 -0300

    Delete questão2614-uri.sql

--- questão2614-uri.sql
+++
@@ -1,6 +0,0 @@
-select customer.name, 
-       rental.rentals_date
-  from customers customer
-  join rentals rental
-    on customer.id = rental.id_customers
- where extract(month from rental.rentals_date) = 9
