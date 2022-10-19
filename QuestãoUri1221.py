dca4bea16d2e748fca77d339e929e365bda96dc5
Bruno-Grangeiro <109104329+Bruno-Grangeiro@users.noreply.github.com>
2022-10-19 20:49:06 -0300

    Create QuestãoUri1221.py

---
+++ QuestãoUri1221.py
@@ -0,0 +1,16 @@
+# -- coding: utf-8 --
+from math import sqrt
+n=int(input())
+x=0
+while x<n:
+    ehprimo=True
+    num=int(input())
+    x=x+1
+    for i in range(2,int(sqrt(num))+1):
+        if num % i ==0:
+            ehprimo=False
+            break
+    if ehprimo:
+        print("Prime")
+    else:
+        print("Not Prime")
